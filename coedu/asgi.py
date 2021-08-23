979b9d2baf9f58189478b29afbce918db05e896b
Seung-eun <tmddms9303@gmail.com>
2021-08-23 14:21:54 +0900

    Initial commit

---
+++ coedu/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for coedu project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coedu.settings')
+
+application = get_asgi_application()
