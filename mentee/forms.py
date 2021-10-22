93246c7c7ed446bf2c169fd78e98eccb175ca96a
Seung-eun <tmddms9303@gmail.com>
2021-10-22 12:23:23 +0900

    signup form

--- mentee/forms.py
+++ mentee/forms.py
@@ -6,9 +6,6 @@ class MentorSignupForm(forms.ModelForm):
     class Meta:
         model = Mentor
         fields = ('major', 'subject', 'teaching_subject', 'introduction')
-        widgets = {
-            "subject": forms.CheckboxSelectMultiple
-        }
         labels = {
             'major': '전공',
             'subject': '멘토링 가능한 분야',
@@ -17,8 +14,16 @@ class MentorSignupForm(forms.ModelForm):
         }
         help_texts = {
             'major': '전공한 과의 이름을 작성해주세요.',
-            'subject': '멘토링 가능한 분야는 전공과 다를 수 있으며, 중복 선택 가능합니다.',
-            'teaching_subject': '멘토링 가능한 과목에 대해 상세하게 작성해주세요.',
-            'introduction': '멘토로서의 자신에 대해 소개해주세요.'
+            'subject': '(멘토링 가능한 분야는 전공과 다를 수 있으며, 중복 선택 가능합니다.)',
+        }
+
+
+        widgets = {
+            'subject': forms.CheckboxSelectMultiple(
+                attrs={'style': 'vertical-align: top;'}),
+            'teaching_subject': forms.Textarea(attrs={'style': 'vertical-align: top;', 'placeholder': '멘토링 가능한 과목에 대해 상세하게 작성해주세요.', 'rows': '10', 'cols': '60'}),
+            'introduction': forms.Textarea(
+                attrs={'style': 'vertical-align: top;', 'placeholder': '멘토로서의 자신에 대해 소개해주세요.', 'rows': '10',
+                       'cols': '60'}),
         }
 
--- user/forms.py
+++ user/forms.py
@@ -23,9 +23,10 @@ class SignUpForm(UserCreationForm):
         help_texts = {
             'name': '정확한 이름을 한글로 입력해주세요.',
             'university': '띄어쓰기 없이 공식적인 대학명을 입력해주세요. ex)성대(X), 성균관대학교(O)',
-            'interest_subject': '멘토링 받고 싶은 관심분야에 대해 자세히 작성해주세요.',
         }
-
+        widgets = {
+            'interest_subject': forms.Textarea(attrs={'style': 'vertical-align: top;', 'placeholder': '멘토링 받고 싶은 관심분야에 대해 자세히 작성해주세요.', 'rows': '10', 'cols': '60'}),
+        }
 
 class MenteeChangeForm(UserChangeForm):
     class Meta:
@@ -39,7 +40,9 @@ class MenteeChangeForm(UserChangeForm):
         help_texts = {
             'name': '정확한 이름을 한글로 입력해주세요.',
             'university': '띄어쓰기 없이 공식적인 대학명을 입력해주세요. ex)성대(X), 성균관대학교(O)',
-            'interest_subject': '멘토링 받고 싶은 관심분야에 대해 자세히 작성해주세요.',
+        }
+        widgets = {
+            'interest_subject': forms.Textarea(attrs={'style': 'vertical-align: top;', 'placeholder': '멘토링 받고 싶은 관심분야에 대해 자세히 작성해주세요.', 'rows': '10', 'cols': '60'}),
         }
 
 
@@ -54,10 +57,19 @@ class MentorChangeForm(forms.ModelForm):
             'teaching_subject': '멘토링 가능한 상세 과목',
             'introduction': '멘토로서 소개'
         }
+
         help_texts = {
             'major': '전공한 과의 이름을 작성해주세요.',
             'subject': '멘토링 가능한 분야는 전공과 다를 수 있으며, 중복 선택 가능합니다.',
-            'teaching_subject': '멘토링 가능한 과목에 대해 상세하게 작성해주세요.',
-            'introduction': '멘토로서의 자신에 대해 소개해주세요.'
+        }
+
+        widgets = {
+            'subject': forms.CheckboxSelectMultiple(
+                attrs={'style': 'vertical-align: top;'}),
+
+            'teaching_subject': forms.Textarea(attrs={'style': 'vertical-align: top;', 'placeholder': '멘토링 가능한 과목에 대해 상세하게 작성해주세요.', 'rows': '10', 'cols': '60'}),
+            'introduction': forms.Textarea(
+                attrs={'style': 'vertical-align: top;', 'placeholder': '멘토로서의 자신에 대해 소개해주세요.', 'rows': '10',
+                       'cols': '60'}),
         }
 
