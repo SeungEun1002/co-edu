191765d6681b98ba394cbe9f9d9041186a759f3d
Seung-eun <tmddms9303@gmail.com>
2021-10-24 00:48:29 +0900

    Debug false

--- coedu/settings/prod.py
+++ coedu/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # TODO: change to False
+DEBUG = False  # TODO: change to False
 
 ALLOWED_HOSTS = ['3.36.220.93', 'co-edu.co.kr']
 
